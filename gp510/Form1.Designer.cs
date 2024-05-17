a88820a68b4064cdf025db7707bf02ae426b00bc
katann-IWATATE-tann <104120192+katann-tann@users.noreply.github.com>
2024-05-17 14:28:16 +0900

    ボタン名変更

---
+++ gp510/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace gp510
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            mybutten = new Button();
+            SuspendLayout();
+            // 
+            // mybutten
+            // 
+            mybutten.BackColor = SystemColors.ActiveCaption;
+            mybutten.Location = new Point(114, 97);
+            mybutten.Margin = new Padding(3, 4, 3, 4);
+            mybutten.Name = "mybutten";
+            mybutten.Size = new Size(237, 233);
+            mybutten.TabIndex = 0;
+            mybutten.Text = "button1";
+            mybutten.UseVisualStyleBackColor = false;
+            mybutten.Click += mybutten_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(914, 540);
+            Controls.Add(mybutten);
+            Font = new Font("メイリオ", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button mybutten;
+    }
+}
---
+++ gp510/Form1.cs
@@ -0,0 +1,15 @@
+namespace gp510
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mybutten_Click(object sender, EventArgs e)
+        {
+            mybutten.Text = "‰Ÿ‚³‚ê‚½";
+        }
+    }
+}
